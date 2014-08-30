8b61a86c77582bb03864bd248b37f47019e4bc45
Thomas Levine <_@thomaslevine.com>
2014-08-30 14:18:26 +0000

    finish initial pass at switching to sqlalchemy

--- warehouse/history.py
+++ warehouse/history.py
@@ -1,41 +1,51 @@
 import os
 
 import sqlalchemy as s
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 from historian_reader.shell import historian
 
-HISTORY = os.path.join(os.path.expanduser('~'), 'history', 'shell')
+Base = declarative_base()
+
+class Shell(Base):
+    __tablename__ = 'shells'
+
+    shell = s.Column(s.String(40), primary_key = True)
+    shell_date = s.Column(s.DateTime(), nullable = False)
+
+    def __repr__(self):
+        return '<Shell(shell = "%s", shell_date = %s)>' % \
+               (self.shell, self.shell_date)
+
+class Command(Base):
+    __tablename__ = 'commands'
+    shell = s.Column(s.String(40), s.ForeignKey('shells.shell'),
+                       nullable=False),
+    command_date = s.Column(s.DateTime, nullable = False),
+    command = s.Column(s.String, nullable = False),
+    def __repr__(self):
+        return '<Command(shell = "%s", command_date = %s, command = """%s""")>' % \
+               (self.shell, self.command_date, self.command)
 
 
-metadata = s.MetaData()
-
-sessions = s.Table('sessions', metadata,
-    s.Column('session', s.String(40), primary_key = True),
-    s.Column('session_date', s.DateTime(), nullable = False),
-)
-
-commands = s.Table('commands', metadata,
-    s.Column('session', s.String(40), s.ForeignKey('sessions.session'),
-             nullable=False),
-    s.Column('command_date', s.DateTime, nullable = False),
-    s.Column('command', s.String, nullable = False),
-)
-
-def connect(cache_directory):
+def session(cache_directory):
     database_file = os.path.join(cache_directory, 'shell-history.sqlite')
     engine = create_engine('sqlite:///' + database_file)
-    metadata.create_all(engine)
+    return sessionmaker(bind=engine)
+   #metadata.create_all(engine)
 
-def update(sessions_table):
-    previous_sessions = sessions_table.distinct('session')
-    for session in historian(directory = HISTORY, skip = previous_sessions):
-        rows = ({
-                    'session': session['session'],
-                    'session_date': session['session_date'],
-                    'command_date': command_date,
-                    'command': command_string,
-                } for command_date, command_string in session['commands'])
-        sessions_table.insert_many(rows)
+def update(session):
+    HISTORY = os.path.join(os.path.expanduser('~'), 'history', 'shell')
+    previous_shells = session.query(Shell.shell)
+    for shell in historian(directory = HISTORY, skip = previous_shells):
+        session.add(Shell(shell = shell['shell'],
+                          shell_date = shell['shell_date']))
+        session.add_all((Command(shell = shell['shell'],
+                                 command_date = command_date,
+                                 command = command_string,
+                ) for command_date, command_string in shell['commands']))
+        session.commit()
 
 def run(cache_directory):
-    update(connect(cache_directory))
+    update(session(cache_directory))
