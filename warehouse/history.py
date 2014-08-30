f75eb8a4f264bad011465f7deb4584dc63fa46a7
Thomas Levine <_@thomaslevine.com>
2014-08-30 13:59:19 +0000

    switching to sqlalchemy

--- warehouse/history.py
+++ warehouse/history.py
@@ -1,19 +1,30 @@
 import os
 
-import dataset
+import sqlalchemy as s
 
 from historian_reader.shell import historian
 
 HISTORY = os.path.join(os.path.expanduser('~'), 'history', 'shell')
 
+
+metadata = s.MetaData()
+
+sessions = s.Table('sessions', metadata,
+    s.Column('session', s.String(40), primary_key = True),
+    s.Column('session_date', s.DateTime(), nullable = False),
+)
+
+commands = s.Table('commands', metadata,
+    s.Column('session', s.String(40), s.ForeignKey('sessions.session'),
+             nullable=False),
+    s.Column('command_date', s.DateTime, nullable = False),
+    s.Column('command', s.String, nullable = False),
+)
+
 def connect(cache_directory):
     database_file = os.path.join(cache_directory, 'shell-history.sqlite')
-    db = dataset.connect('sqlite:///' + database_file)
-    if 'sessions' not in db.tables:
-        sessions = db.create_table('sessions', primary_id = 'session')
-    else:
-        sessions = db['sessions']
-    return sessions
+    engine = create_engine('sqlite:///' + database_file)
+    metadata.create_all(engine)
 
 def update(sessions_table):
     previous_sessions = sessions_table.distinct('session')
