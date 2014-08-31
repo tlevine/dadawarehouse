4e08232e685d66fac4342455579c4e6d2ba2a281
Thomas Levine <_@thomaslevine.com>
2014-08-31 15:39:29 +0000

    unioning

--- warehouse/facebookchat.py
+++ warehouse/facebookchat.py
@@ -3,7 +3,6 @@ import shutil
 import subprocess
 
 import sqlalchemy
-import bubbles
 
 WAREHOUSE = os.path.expanduser('~/.dadawarehouse')
 LOCAL_CHAT = os.path.join(WAREHOUSE, 'facebookchat')
@@ -11,7 +10,7 @@ REMOTE_CHAT = 'safe:rsync/ekg2-logs/xmpp:perluette@chat.facebook.com/*.db'
 RSYNC = ['rsync', '--archive', '--sparse'] #, '--verbose']
 
 RESULT = 'sqlite:////home/tlevine/.dadawarehouse/dada.sqlite'
-TMP = 'sqlite:////tmp/facebookchat.sqlite'
+TMP = '/tmp/facebookchat.sqlite' 
 
 def download():
     if not os.path.isdir(LOCAL_CHAT):
@@ -19,27 +18,29 @@ def download():
     scp = subprocess.Popen(RSYNC + [REMOTE_CHAT, LOCAL_CHAT])
     scp.wait()
 
-def tmpdb():
-    if not os.path.exists(TMP.replace('sqlite:///', '')):
-        engine = sqlalchemy.create_engine(TMP)
-        SCHEMA = [
-            'CREATE TABLE log_msg (session TEXT, uid TEXT, nick TEXT, type TEXT, sent INT, ts INT, sentts INT, body TEXT);',
-            'CREATE TABLE log_status (session TEXT, uid TEXT, nick TEXT, ts INT, status TEXT, desc TEXT);',
-            'CREATE INDEX ts ON log_msg(ts);',
-            'CREATE INDEX uid_ts ON log_msg(uid, ts);',
-        ]
-        for command in SCHEMA:
-            engine.execute(command)
-    return bubbles.open_store("sql", TMP)
+def union(sources:str, destination:str):
+    sql = '''\
+CREATE TABLE log_msg (session TEXT, uid TEXT, nick TEXT, type TEXT, sent INT, ts INT, sentts INT, body TEXT);
+CREATE TABLE log_status (session TEXT, uid TEXT, nick TEXT, ts INT, status TEXT, desc TEXT);
+CREATE INDEX ts ON log_msg(ts);
+CREATE INDEX uid_ts ON log_msg(uid, ts);'''
+    for source in sources:
+        sql += '''\
+ATTACH DATABASE '%s' AS today;
+BEGIN TRANSACTION;
+INSERT INTO log_msg SELECT * FROM today.log_msg;
+INSERT INTO log_status SELECT * FROM today.log_status;
+COMMIT TRANSACTION;
+DETACH DATABASE today;
+''' % source
+    sqlite3 = subprocess.Popen(['sqlite3', destination], stdin = subprocess.PIPE)
+    sqlite3.stdin.write(sql.encode('latin1'))
+    sqlite3.wait()
 
 def update(session):
-    day_log = 'sqlite:////home/tlevine/.dadawarehouse/facebookchat/2013-11-25.db'
-    log = bubbles.open_store('sql', day_log)
-
-    p = bubbles.Pipeline(stores = {'tmp': tmpdb(), 'log': log})
-    p.source('log', 'log_msg')
-    p.insert_into('tmp', 'log_msg')
-    p.run()
-
-
-#   download()
+  # download()
+    daily_logs = (os.path.join(LOCAL_CHAT, fn) for fn in os.listdir(LOCAL_CHAT))
+    if os.path.exists(TMP):
+        raise ValueError(TMP + ' exists')
+    else:
+        union(daily_logs, TMP)
