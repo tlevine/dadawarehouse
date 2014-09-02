87b89060a8302c0d0f479596a8aebb907191b856
Thomas Levine <_@thomaslevine.com>
2014-09-02 21:28:18 +0000

    separate the integration tests

--- doeund/test/test_inference.py
+++ doeund/test/test_inference.py
@@ -7,7 +7,7 @@ from doeund.inference import fact_measures
 Table = namedtuple('Table', ['columns'])
 Column = namedtuple('Column', ['name', 'foreign_keys'])
 
-def test_fact_measures_fake():
+def test_fact_measures():
     name = Column('name', set())
     height = Column('height', set())
     city = Column('city', {'Pretend this is a city column.'})
@@ -16,11 +16,3 @@ def test_fact_measures_fake():
     observed = fact_measures(table)
     expected = {'name': [name], 'height': [height]}
     n.assert_dict_equal(observed, expected)
-
-def test_fact_measures_real():
-    import warehouse.main
-    cube = warehouse.main.query()['fact_calendarevent']
-
-    observed = list(fact_measures(cube._args[1]).keys())
-    expected = ['event_id', 'event_date', 'event_description']
-    n.assert_list_equal(observed, expected)
