aab4c5112279983396464fba09b194e7bb33c5ff
Thomas Levine <_@thomaslevine.com>
2014-09-02 16:54:24 +0000

    start a model provider

--- doeund/__init__.py
+++ doeund/__init__.py
@@ -1,4 +1,12 @@
+from sqlalchemy.orm import sessionmaker
+
 from .components import Fact, Dimension, Column, Base as _Base
 from .provider import model
 
-def doeund
+def doeund(engine,
+    '''
+    engine: SQLAlchemy engine, from sqlalchemy.create_engine
+
+    '''
+    _Base.metadata.create_all(engine) 
+    session = sessionmaker(bind=engine)()
--- doeund/provider.py
+++ doeund/provider.py
@@ -1,3 +1,19 @@
 from cubes import ModelProvider as _ModelProvider
 
 class Model(_ModelProvider):
+    def __init__(self, metadata = None, declarative_base = None):
+        super(Model, self).__init__(metadata)
+        if declarantive_base == None:
+            raise TypeError('declarative_base must be set.')
+        else:
+            self.Base = declarative_base
+
+    def list_cubes(self):
+    # return a list of cubes that the provider provides. Return value should be a dictionary with keys: name, label, description and info.
+
+    def cube(self, name):
+    # return a cubes.Cube object
+
+    def dimension(self, name, dimensions):
+    # return a cubes.Dimension object. dimensions is a dictionary of public dimensions that can be used as templates. If a template is missing the method should raise TemplateRequired(template) error.
+
