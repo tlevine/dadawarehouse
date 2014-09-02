744ae85fb7ab672726dd449a01298fe7945fe627
Thomas Levine <_@thomaslevine.com>
2014-09-02 20:21:21 +0000

    debugging cli

--- warehouse/main.py
+++ warehouse/main.py
@@ -2,14 +2,13 @@ import os
 
 from doeund import doeund
 
-import .db as db
-import .history as history
-import .pal as pal
-import .facebookchat as fb
+from .history import update as history
+from .pal import update as pal
+from .facebookchat import update as fb
 
 CACHE_DIRECTORY = os.path.expanduser('~/.dadawarehouse')
 
-def connect(cache_directory)
+def connect(cache_directory):
     database_file = os.path.join(cache_directory, 'dada.sqlite')
     engine = s.create_engine('sqlite:///' + database_file)
     return doeund(engine)
@@ -18,9 +17,9 @@ def load():
     if not os.path.isdir(CACHE_DIRECTORY):
         os.mkdir(CACHE_DIRECTORY)
     session, _ = connect(CACHE_DIRECTORY)
-   #fb.update(session)
-   #history.update(session)
-    pal.update(session)
+   #fb(session)
+   #history(session)
+    pal(session)
 
 def query():
     session, cubes = connect(CACHE_DIRECTORY)
