2e9c2b706a22bb636c72e07da051ead1b733c2dc
Thomas Levine <_@thomaslevine.com>
2014-09-02 20:35:27 +0000

    move the mkdir

--- warehouse/main.py
+++ warehouse/main.py
@@ -11,13 +11,13 @@ from .facebookchat import update as fb
 CACHE_DIRECTORY = os.path.expanduser('~/.dadawarehouse')
 
 def connect(cache_directory):
+    if not os.path.isdir(cache_directory):
+        os.mkdir(cache_directory)
     database_file = os.path.join(cache_directory, 'dada.sqlite')
     engine = create_engine('sqlite:///' + database_file)
     return doeund(engine)
 
 def load():
-    if not os.path.isdir(CACHE_DIRECTORY):
-        os.mkdir(CACHE_DIRECTORY)
     session, _ = connect(CACHE_DIRECTORY)
    #fb(session)
    #history(session)
