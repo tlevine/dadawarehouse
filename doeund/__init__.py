33a0dc6010fa33cb37e6a863e8279b99e19b676d
Thomas Levine <_@thomaslevine.com>
2014-09-02 17:55:24 +0000

    -

--- doeund/__init__.py
+++ doeund/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from .components import Fact, Dimension, Column, Base as _Base
 from .provider import _DoeundProvider
 
-def doeund(engine):
+def doeund(engine, fact_tables, dimension_tables):
     '''
     After assembling your schema with the Fact, Dimension, and
     Column classes from doeund, produce a database session and
@@ -13,7 +13,9 @@ def doeund(engine):
     '''
     class DoeundProvider(_DoeundProvider):
         def __init__(self, metadata = None):
-            super(_DoeundProvider, self).__init__(metadata = metadata, declarative_base = _Base)
+            super(_DoeundProvider, self).__init__(metadata = metadata,
+                declarative_base = _Base, fact_tables = fact_tables,
+                dimension_tables = dimension_tables)
 
     _Base.metadata.create_all(engine) 
     session = sessionmaker(bind=engine)()
