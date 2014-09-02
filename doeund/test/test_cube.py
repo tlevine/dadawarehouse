3b38d385fb2dfc2a738737257ee832db34d8c016
Thomas Levine <_@thomaslevine.com>
2014-09-02 21:26:26 +0000

    fix fixture

--- doeund/test/test_cube.py
+++ doeund/test/test_cube.py
@@ -6,5 +6,5 @@ cube = warehouse.main.query()['fact_calendarevent']
 
 def test_dimensions():
     observed = list(cube.dimensions.keys())
-    expected = ['calendarfile', 'event_description']
+    expected = ['event_id', 'event_date', 'event_description', 'calendarfile']
     n.assert_list_equal(observed, expected)
