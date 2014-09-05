0ad922ff1953e7539191d987ff0dc110d749c6b3
Thomas Levine <_@thomaslevine.com>
2014-09-05 04:22:39 +0000

    cuts

--- doeund/cube.py
+++ doeund/cube.py
@@ -69,16 +69,7 @@ class Cube:
     def __repr__(self):
         return '<Cube for table "%s">' % self._args[1]
 
-    def point_cut(self, dimension, path):
-        # Copy the query
-        query = self._query.all()
-
-        # Drill down as deep as the path allows.
-        for level, value in zip(self.dimensions[dimension], path):
-            query = self._query.filter(level == value)
-        return query
-
-    def set_cut(self, dimension, paths):
+    def set_cut(self, dimension, *paths):
         def match_path(dimension, path):
             # An AND-joined query to match the full path
             return and_(*(level == value for level, value in \
