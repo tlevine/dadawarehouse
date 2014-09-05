cb6573f2be47d32c884ccae352c9e175864e02e3
Thomas Levine <_@thomaslevine.com>
2014-09-05 21:18:00 +0000

    write tests

--- warehouse/twitter/load.py
+++ warehouse/twitter/load.py
@@ -1,4 +1,5 @@
 import os, subprocess, json
+import re
 import datetime
 
 NOTMUCH = ['notmuch', 'show', '--format=json', 'from:twitter.com']
@@ -20,4 +21,22 @@ def update(session):
     for email in emails():
         subject = email[0][0]['headers']['Subject']
         date = datetime.datetime.fromtimestamp(email[0][0]['timestamp'])
-        print(subject)
+        parse_subject(subject)
+
+class actions:
+    followed = re.compile(r'(^[^(]+)\(@([^)]+) is now following you on Twitter!$')
+    mentioned = re.compile(r'(^[^(]+)\(@([^)]+) mentioned you on Twitter!$')
+    replied = re.compile(r'(^[^(]+)\(@([^)]+) replied to one of your Tweets!')
+    favorited = re.compile(r'(^[^(]+)\(@([^)]+) favorited one of your Tweets!')
+    multiple = re.compile('Thomas Levine, you have new followers on Twitter!')
+
+def parse_subject(subject):
+    for action in [actions.followed, actions.mentioned,
+                   actions.replied, actions.favorited]:
+        m = re.match(action, subject)
+        if m:
+            return m.group(1), m.group(2), action
+    if re.match(actions.multiple, subject):
+        return None, None, actions.multiple
+    else:
+        raise ValueError('Could not parse subject "%s"' % subject)
--- warehouse/twitter/test/test_load.py
+++ warehouse/twitter/test/test_load.py
@@ -2,6 +2,13 @@ import nose.tools as n
 
 from ..load import parse_subject, actions
 
+def check_parse_subject(testcase):
+    subject, *expected = testcase
+    n.assert_tuple_equal(parse_subject(subject), tuple(expected))
+
+def test_parse_subject():
+    for testcase in testcases:
+        yield check_parse_subject, testcase
 testcases = [
     ('Rebecca Williams (@internetrebecca) is now following you on Twitter!',
         'Rebecca Williams', 'internetrebecca', actions.followed),
