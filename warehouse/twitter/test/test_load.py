98d54a4989bac32a2414b7df0d027a13007952ea
Thomas Levine <_@thomaslevine.com>
2014-09-05 21:07:05 +0000

    some tests

--- warehouse/twitter/test/test_load.py
+++ warehouse/twitter/test/test_load.py
@@ -1,23 +1,16 @@
 import nose.tools as n
 
-from ..load import parse_subject
+from ..load import parse_subject, actions
 
-'''
-Rebecca Williams (@internetrebecca) is now following you on Twitter!
-Volkan Unsal (@picardo) mentioned you on Twitter!
-Albert Sun (@albertsun) is now following you on Twitter!
-Aidan (@subtlethoughts) mentioned you on Twitter!
-Pernilla Näsfors (@pernillan) mentioned you on Twitter!
-Jake Porway (@jakeporway) mentioned you on Twitter!
-Pernilla Näsfors (@pernillan) mentioned you on Twitter!
-Burton DeWilde (@bjdewilde) replied to one of your Tweets!
-Burton DeWilde (@bjdewilde) favorited one of your Tweets!
-Susannah Fox (@SusannahFox) replied to one of your Tweets!
-Thomas Levine, you have new followers on Twitter!
-Aaron Schumacher (@planarrowspace) mentioned you on Twitter!
-Thomas Levine, you have a new follower on Twitter!
-Sebastian Bruce (@brucesebi) is now following you on Twitter!
-Burton DeWilde (@bjdewilde) favorited one of your Tweets!
-
-
-'''
+testcases = [
+    ('Rebecca Williams (@internetrebecca) is now following you on Twitter!',
+        'Rebecca Williams', 'internetrebecca', actions.followed),
+    ('Volkan Unsal (@picardo) mentioned you on Twitter!',
+        'Volkan Unsal', 'picardo', actions.mentioned),
+    ('Burton DeWilde (@bjdewilde) replied to one of your Tweets!',
+        'Burton DeWilde', 'bjdewilde', actions.replied),
+    ('Burton DeWilde (@bjdewilde) favorited one of your Tweets!',
+        'Burton DeWilde', 'bjdewilde', actions.favorited),
+    ('Thomas Levine, you have new followers on Twitter!',
+        None, None, actions.multiple),
+]
