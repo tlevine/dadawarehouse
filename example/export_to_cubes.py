adb00e93e5cd008c37cb8aaf1729551b58a40f9a
Thomas Levine <_@thomaslevine.com>
2014-09-07 05:57:47 +0000

    export example

--- example/export_to_cubes.py
+++ example/export_to_cubes.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+import json
+
 import sqlalchemy
 
 from doeund import create
@@ -7,3 +10,5 @@ import warehouse.pal.model
 
 engine = sqlalchemy.create_engine("sqlite:///")
 session, model = create(engine)
+with open('/tmp/model.json', 'w') as fp:
+    json.dump(model, fp)
