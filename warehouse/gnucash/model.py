a024ffb814c5bb1027a4645df758792645d196d6
Thomas Levine <_@thomaslevine.com>
2014-09-07 08:28:01 +0000

    guncash

--- warehouse/gnucash/model.py
+++ warehouse/gnucash/model.py
@@ -4,27 +4,41 @@ import warehouse.model as m
 
 Guid = s.String(32)
 
+class GnuCashAccountType(Dimension):
+    pk = m.PkColumn()
+    account_type = m.LabelColumn()
+
+class GnuCashSection(Dimension):
+    pk = m.PkColumn()
+    section = m.LabelColumn()
+
+class GnuCashSubSection(Dimension):
+    pk = m.PkColumn()
+    subsection = m.LabelColumn()
+
 class GnuCashAccount(Dimension):
     guid = m.Column(Guid, primary_key = True)
 
     name = m.Column(s.String)
     code = m.Column(s.String)
     description = m.Column(s.String)
-
     commodity_guid = m.Column(Guid)
 
-class GnuCashHierarchy(Dimension):
-    guid = m.Column(Guid, primary_key = True)
-
-    account = m.Column(Guid, s.ForeignKey(GnuCashAccount.guid))
-    section = m.Column(Guid, s.ForeignKey(GnuCashAccount.guid))
-    subsection = m.Column(Guid, s.ForeignKey(GnuCashAccount.guid), nullable = True)
+    account_type = m.FkColumn(GnuCashAccountType.pk))
+    section = m.FkColumn(GnuCashSection.pk))
+    subsection = m.FkColumn(GnuCashSubSection.pk), nullable = True)
 
 class GnuCashTransaction(Dimension):
+    guid = m.Column(Guid, primary_key = True)
+    currency = m.Column(Guid)
+    post_date = m.DateTimeColumn()
+    enter_date = m.DateTimeColumn()
+    description = m.Column(s.String)
 
 class GnuCashSplit(Fact):
     guid = m.Column(Guid, primary_key = True)
-    account_guid = m.Column(Guid, s.ForeignKey(Account.guid))
+    account_guid = m.Column(Guid, s.ForeignKey(GnuCashAccount.guid))
+    transaction_guid = m.Column(Guid, s.ForeignKey(GnuCashTransaction.guid))
     memo = m.Column(s.String)
     value_num = m.Column(s.BigInteger)
     value_denom = m.Column(s.BigInteger)
