f8323ff9731f8d74ac186b4cde5c92ffa5916e42
Thomas Levine <_@thomaslevine.com>
2014-09-07 20:05:16 +0000

    notmuch

--- warehouse/notmuch/model.py
+++ warehouse/notmuch/model.py
@@ -5,21 +5,32 @@ import doeund as d
 
 import warehouse.model as m
 
+class Person(d.Dimension):
+    pk = PkColumn()
+    name = d.Column(s.String)
+
+class Address(d.Dimension):
+    pk = d.Column(s.String, primary_key = True)
+    person_id = d.FkColumn(Person.pk)
+
 class Message(d.Dimension):
     pk = d.Column(s.String, primary_key = True)
-    date_id = m.DateColumn()
+    datetime_id = m.DateTimeColumn()
     thread_id = d.Column(s.String, s.ForeignKey(Thread))
+    filename = d.Column(s.String)
+    subject = d.Column(s.String)
+    from_address = d.FkColumn(Address.pk)
+
+class NotmuchCorrespondance(d.Fact):
+    '''
+    to_address includes CC, BCC
+    '''
+    pk = d.PkColumn()
+    from_address = d.Column(s.String, s.ForeignKey(Address.pk))
+    to_address = d.Column(s.String, s.ForeignKey(Address.pk))
 
 class NotmuchMessage(d.Fact):
-    pk = m.PkColumn()
-
-    # Could be unique identifiers
-    message_id = d.Column(s.String, s.ForeignKey(Message.pk))
-    filename = d.Column(s.String)
-
-    # Foreign Keys
-    date_id = m.DateColumn()
-    thread_id = d.Column(s.String, s.ForeignKey(Thread))
+    pk = d.Column(s.String, primary_key = True)
 
 class NotmuchMessagePart(d.Fact):
     message_id = d.Column(s.String, s.ForeignKey(Message.pk), primary_key = True)
