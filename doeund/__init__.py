4d7007d113221c5d5baee9f51bab130e3aa9741b
Thomas Levine <_@thomaslevine.com>
2014-09-07 06:13:57 +0000

    shell stuff

--- doeund/__init__.py
+++ doeund/__init__.py
@@ -3,8 +3,9 @@ from sqlalchemy.orm import sessionmaker as _sessionmaker
 from .database import Fact, Dimension, Column, Base as _Base, merge_on_unique
 from .export import export as _export
 
-def create(engine):
+def database(engine):
     _Base.metadata.create_all(engine) 
-    session = _sessionmaker(bind=engine)()
-    model = _export(_Base.metadata.tables)
-    return session, model
+    return _sessionmaker(bind=engine)()
+
+def model():
+    return _export(_Base.metadata.tables)
--- warehouse/main.py
+++ warehouse/main.py
@@ -1,8 +1,9 @@
 import os
+import json
 
 from sqlalchemy import create_engine
 
-from doeund import doeund
+import doeund
 
 from warehouse.history.load import update as history
 from warehouse.pal.load import update as pal
@@ -10,19 +11,16 @@ from warehouse.facebookchat.load import update as fb
 
 CACHE_DIRECTORY = os.path.expanduser('~/.dadawarehouse')
 
-def load():
+def load_data():
   # engine = create_engine('postgres:///tlevine')
     engine = create_engine('sqlite:////tmp/dada.sqlite')
-    session, _ = doeund(engine)
+    session = doeund.database(engine)
     fb(session)
     history(session)
     pal(session)
 
-def example():
-    _, cubes = doeund(create_engine('postgres:///tlevine'))
-    cube = cubes['fact_facebookchatstatuschange'] #.dimensions['facebookuser']
-    print('When Thomas Levines went online and offline:')
-    print(cube.point_cut('facebookuser', ['Thomas Levine']).all())
-
-if __name__ == '__main__':
-    session, cubes = doeund(create_engine('postgres:///tlevine'))
+def export_model():
+    engine = create_engine("sqlite:///")
+    model = doeund.model()
+    with open('/tmp/model.json', 'w') as fp:
+        json.dump(model, fp)
