521530a83e369cf374be46cf056de4861d976edb
Thomas Levine <_@thomaslevine.com>
2014-09-07 04:11:53 +0000

    keep the present querying as a query file

--- doeund/__init__.py
+++ doeund/__init__.py
@@ -2,23 +2,30 @@ from sqlalchemy.orm import sessionmaker as _sessionmaker
 from sqlalchemy.exc import ProgrammingError
 
 from .database import Fact, Dimension, Column, Base as _Base, merge_on_unique
-from .cube import Cube
+from .query import Cube
+from .export import export as _export
 
-def doeund(engine, refresh = False):
+def refresh(engine):
+    for table in _Base.metadata.sorted_tables:
+        try:
+            engine.execute(table.delete())
+        except ProgrammingError:
+            pass
+
+def create(engine):
+    _Base.metadata.create_all(engine) 
+    return _sessionmaker(bind=engine)()
+
+def query(engine):
     '''
     After assembling your schema with the Fact, Dimension, and
     Column classes from doeund, produce some cubes.
 
     engine: SQLAlchemy engine, from sqlalchemy.create_engine
     '''
-    if refresh:
-        for table in _Base.metadata.sorted_tables:
-            try:
-                engine.execute(table.delete())
-            except ProgrammingError:
-                pass
-    _Base.metadata.create_all(engine) 
-    session = _sessionmaker(bind=engine)()
     cubes = {name: Cube(session, table) for name, table in \
              _Base.metadata.tables.items() if name.startswith('fact_')}
     return session, cubes
+
+def export():
+    return _export(_Base.metadata.tables)
