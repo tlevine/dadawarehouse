7962637f1fba868f6250e956de23053fd0346082
Thomas Levine <_@thomaslevine.com>
2014-09-07 08:16:33 +0000

    more modeling

--- warehouse/gnucash/model.py
+++ warehouse/gnucash/model.py
@@ -4,19 +4,25 @@ import warehouse.model as m
 
 Guid = s.String(32)
 
-class GnucashAccount(Dimension):
+class GnuCashAccount(Dimension):
     guid = m.Column(Guid, primary_key = True)
 
     name = m.Column(s.String)
     code = m.Column(s.String)
     description = m.Column(s.String)
 
-    account_type = m.Column(s.String)
     commodity_guid = m.Column(Guid)
 
-    parent_guid = m.Column(Guid, s.ForeignKey(GnucashAccount.guid))
+class GnuCashHierarchy(Dimension):
+    guid = m.Column(Guid, primary_key = True)
 
-class GnucashSplit(Dimension):
+    account = m.Column(Guid, s.ForeignKey(GnuCashAccount.guid))
+    section = m.Column(Guid, s.ForeignKey(GnuCashAccount.guid))
+    subsection = m.Column(Guid, s.ForeignKey(GnuCashAccount.guid), nullable = True)
+
+class GnuCashTransaction(Dimension):
+
+class GnuCashSplit(Fact):
     guid = m.Column(Guid, primary_key = True)
     account_guid = m.Column(Guid, s.ForeignKey(Account.guid))
     memo = m.Column(s.String)
