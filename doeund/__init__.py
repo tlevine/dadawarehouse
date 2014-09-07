0110ec3c0015bcf1b6102f9316b912087db18a0c
Thomas Levine <_@thomaslevine.com>
2014-09-07 04:40:36 +0000

    cut stuff

--- doeund/__init__.py
+++ doeund/__init__.py
@@ -2,22 +2,10 @@ from sqlalchemy.orm import sessionmaker as _sessionmaker
 from sqlalchemy.exc import ProgrammingError
 
 from .database import Fact, Dimension, Column, Base as _Base, merge_on_unique
-# from .export import export as _export
+from .export import export as _export
 
 def create(engine):
     _Base.metadata.create_all(engine) 
-    return _sessionmaker(bind=engine)()
-
-def query(engine):
-    '''
-    After assembling your schema with the Fact, Dimension, and
-    Column classes from doeund, produce some cubes.
-
-    engine: SQLAlchemy engine, from sqlalchemy.create_engine
-    '''
-    cubes = {name: Cube(session, table) for name, table in \
-             _Base.metadata.tables.items() if name.startswith('fact_')}
-    return session, cubes
-
-def export():
-    return _export(_Base.metadata.tables)
+    session = _sessionmaker(bind=engine)()
+    model = _export(_Base.metadata.tables)
+    return session, model
