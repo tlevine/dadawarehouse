d6be267c5fb1474fb5f0811dd42803e5bf6f202a
Thomas Levine <_@thomaslevine.com>
2014-09-08 00:33:10 +0000

    link methods

--- warehouse/notmuch/model.py
+++ warehouse/notmuch/model.py
@@ -8,9 +8,13 @@ import warehouse.model as m
 class Address(d.Dimension):
     pk = m.Column(s.String, primary_key = True)
     name = m.Column(s.String)
+    def link(self, session):
+        return session.merge(self)
 
 class Thread(d.Dimension):
     pk = m.Column(s.String, primary_key = True)
+    def link(self, session):
+        return session.merge(self)
 
 class Message(d.Dimension):
     pk = m.Column(s.String, primary_key = True)
@@ -20,23 +24,40 @@ class Message(d.Dimension):
     thread = relationship(Thread)
     filename = m.Column(s.String)
     subject = m.Column(s.String)
-    from_address = m.Column(s.String, s.ForeignKey(Address.pk))
+    from_address_id = m.Column(s.String, s.ForeignKey(Address.pk))
+    from_address = relationship(Address)
+    def link(self, session):
+        self.from_address = self.from_address.link(session)
+        return session.merge(self)
 
 class NotmuchCorrespondance(d.Fact):
     '''
     to_address includes CC, BCC
     '''
     pk = m.PkColumn()
-    from_address = m.Column(s.String, s.ForeignKey(Address.pk))
-    to_address = m.Column(s.String, s.ForeignKey(Address.pk))
+    from_address_id = m.Column(s.String, s.ForeignKey(Address.pk))
+    from_address = relationship(Address)
+    to_address_id = m.Column(s.String, s.ForeignKey(Address.pk))
+    from_address = relationship(Address)
+    def link(self, session):
+        self.from_address = self.from_address.link(session)
+        self.to_address = self.to_address.link(session)
+        return self
 
 class NotmuchMessage(d.Fact):
     pk = m.Column(s.String, s.ForeignKey(Message.pk), primary_key = True)
     message = relationship(Message)
+    def link(self, session):
+        self.message = self.message.link(session)
+        return session.merge(self)
 
 class ContentType(d.Dimension):
     pk = m.PkColumn()
     content_type = m.LabelColumn()
+    def link(self, session):
+        return d.merge_on_unique(ContentType, session,
+                                 ContentType.content_type,
+                                 self.content_type)
 
 class NotmuchAttachment(d.Fact):
     message_id = m.Column(s.String, s.ForeignKey(Message.pk), primary_key = True)
@@ -45,3 +66,7 @@ class NotmuchAttachment(d.Fact):
     content_type = relationship(ContentType)
     part_number = m.Column(s.Integer, primary_key = True)
     name = m.Column(s.String)
+    def link(self, session):
+        self.message = self.message.link(session)
+        self.content_type = self.content_type.link(session)
+        return session.merge(self)
