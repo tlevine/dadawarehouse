e0cdb6dc4941361a4312765fbc8b9f10619cb34f
Thomas Levine <_@thomaslevine.com>
2014-09-11 03:48:54 +0000

    debug

--- warehouse/model/util.py
+++ warehouse/model/util.py
@@ -1,5 +1,9 @@
 def d(function):
     'Make a "default" function.'
     def default(context):
-        return function(context.current_parameters['pk'])
+        try:
+            return function(context.current_parameters['pk'])
+        except:
+            print(dict(context.current_parameters))
+            raise
     return default
