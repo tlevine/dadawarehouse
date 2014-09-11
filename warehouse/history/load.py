88d8428db0071c38ddbed40e10a6ebb008bcab6f
Thomas Levine <_@thomaslevine.com>
2014-09-11 17:28:37 +0000

    another

--- warehouse/history/load.py
+++ warehouse/history/load.py
@@ -11,12 +11,17 @@ def update(session):
     HISTORY = os.path.join(os.path.expanduser('~'), 'history', 'shell')
     previous_shells = (row[0] for row in session.query(ShellSession.filename))
     for log in historian(directory = HISTORY, skip = previous_shells):
-        shell_session = ShellSession(filename = log['session'],
-                                     datetime_id = log['session_date']).merge(session)
+        shell_session = session.merge(ShellSession(
+            filename = log['session'], datetime_id = log['session_date']))
         for command_datetime, command_string in log['commands']:
-            command_body = CommandBody(full_command = command_string)
-            command = Command(datetime = m.DateTime.new(command_datetime),
+            command_body = CommandBody.from_label(command_string)
+            command = Command(datetime_id = command_datetime,
                               shellsession = shell_session,
-                              command = command_body).merge(session)
+                              command = command_body)
+            shell_session.commands.append(command)
+
+        session.add(shell_session)
+        ShellSession.create_related(session)
+        Command.create_related(session)
         session.commit()
         logger.info('Inserted commands from shell "%s"' % log['session'])
