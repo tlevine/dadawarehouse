3c06e0c6142767892811d0e3747340ad5d70c8b8
Thomas Levine <_@thomaslevine.com>
2014-09-14 14:19:54 +0000

    separate parsers

--- warehouse/pal/load.py
+++ warehouse/pal/load.py
@@ -4,6 +4,7 @@ import datetime
 from ..logger import logger
 import warehouse.model as m
 from .model import File, CalendarEvent, Description
+from .parsers import entry, read_date, dates
 
 CALENDARS = [os.path.join(os.path.expanduser('~/.pal'), rest) for rest in [\
     'secrets-nsa/secret-calendar.pal',
@@ -37,46 +38,17 @@ def update(session, calendars = CALENDARS):
                     pass
                 elif calendar_file == None:
                     calendar_code, _, calendar_description = line.partition(' ')
-                    calendar_file = File(pk = calendar_code,
+                    todo.append(File(pk = calendar_code,
                         filename = filename,
-                        description = calendar_description)
-                    todo.append(calendar_file)
+                        description = calendar_description))
                 else:
                     for date, description in entry(line):
                         description_id = Description.from_label(session, calendar_description)
-                        todo.append(CalendarEvent(file = calendar_file,
+                        todo.append(CalendarEvent(
+                            file_id = calendar_code,
                             date_id = date,
                             description_id = description_id))
         session.add_all(todo)
         CalendarEvent.create_related(session)
         session.commit()
         logger.info('Inserted events from calendar %s' % filename)
-
-def entry(line):
-    'Read a pal calendar entry'
-    datespec, _, description = line.partition(' ')
-    for date in dates(datespec):
-        yield date, description
-
-def read_date(datestring):
-    try:
-        date = datetime.datetime.strptime(datestring, '%Y%m%d')
-    except ValueError:
-        return None
-    else:
-        return date.date()
-
-def dates(datespec:str):
-    single_date = read_date(datespec)
-    if single_date != None:
-        yield single_date
-    elif datespec.count(':') == 2:
-        subset, start, end = datespec.split(':')
-        if subset == 'DAILY':
-            today = read_date(start)
-            end = read_date(end)
-            while today <= end:
-                yield today
-                today += datetime.timedelta(days = 1)
-    else:
-        logger.warn('Unsupported date specification: "%s"' % datespec)
---
+++ warehouse/pal/parsers.py
@@ -0,0 +1,30 @@
+import datetime
+
+def entry(line):
+    'Read a pal calendar entry'
+    datespec, _, description = line.partition(' ')
+    for date in dates(datespec):
+        yield date, description
+
+def read_date(datestring):
+    try:
+        date = datetime.datetime.strptime(datestring, '%Y%m%d')
+    except ValueError:
+        return None
+    else:
+        return date.date()
+
+def dates(datespec:str):
+    single_date = read_date(datespec)
+    if single_date != None:
+        yield single_date
+    elif datespec.count(':') == 2:
+        subset, start, end = datespec.split(':')
+        if subset == 'DAILY':
+            today = read_date(start)
+            end = read_date(end)
+            while today <= end:
+                yield today
+                today += datetime.timedelta(days = 1)
+    else:
+        logger.warn('Unsupported date specification: "%s"' % datespec)
