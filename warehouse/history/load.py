cb6690c4be270a0746e3ed893cfbf7ba21e13b37
Thomas Levine <_@thomaslevine.com>
2014-09-14 15:07:02 +0000

    tidy the history

--- warehouse/history/load.py
+++ warehouse/history/load.py
@@ -12,8 +12,8 @@ def update(session):
     previous_shells = (row[0] for row in session.query(ShellSession.filename))
     for log in historian(directory = HISTORY, skip = previous_shells):
         todo = []
-        shell_session = session.merge(ShellSession(
-            filename = log['session'], datetime_id = log['session_date']))
+        todo.append(ShellSession(filename = log['session'],
+                                 datetime_id = log['session_date']))
         for command_datetime, command_string in log['commands']:
             command_id = CommandBody.from_label(session, command_string)
             command = Command(datetime_id = command_datetime,
@@ -21,10 +21,8 @@ def update(session):
                               command_id = command_id)
             todo.append(command)
 
-        session.add(shell_session)
-        ShellSession.create_related(session)
-
         session.add_all(todo)
+        ShellSession.create_related(session)
         Command.create_related(session)
 
         logger.info('Inserted commands from shell "%s"' % log['session'])
