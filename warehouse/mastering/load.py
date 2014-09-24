4e75a7083a2ec7c0adc916d258b1505ce3dad649
Thomas Levine <_@thomaslevine.com>
2014-09-24 21:32:45 +0000

    mastering facebook

--- warehouse/mastering/load.py
+++ warehouse/mastering/load.py
@@ -1,7 +1,7 @@
 from collections import Counter
 
 from pylev import levenshtein
-from sqlalchemy import func as f
+from unidecode import unidecode
 
 from ..logger import logger
 from ..facebookchat.model import FacebookMessage, FacebookChatStatusChange
@@ -12,8 +12,8 @@ from .model import Person, EmailAddress
 
 def update(session):
     # _mutt(session)
-    _fb(FacebookChatStatusChange, session)
     # _fb(FacebookMessage, session)
+    _fb(FacebookChatStatusChange, session)
 
 def _mutt(session):
     # Start with mutt aliases.
@@ -22,6 +22,8 @@ def _mutt(session):
         if alias.email_address not in person.email_addresses:
             session.merge(EmailAddress(person_id = alias.pk,
                                        email_address = alias.email_address))
+            logger.info('Added %s' % alias.pk)
+    session.commit()
 
 def _fb(Class, session):
     '''
@@ -33,15 +35,16 @@ def _fb(Class, session):
     '''
     q = session.query(Class.user_id, Class.current_name)\
                .distinct()
-    completed = defaultdict(lambda: set())
+    completed = set()
     for user_id, name in q:
         logger.debug('Checking Facebook user %d' % user_id)
-        if name in completed and user_id not in completed[name]:
-            logger.info('Skipping "%s" because multiple people have the name' % name)
+        if name in completed:
+            logger.info('Skipping "%s" because someone else had that name too' % name)
         else:
-            person_id = name.lower().replace(' ', '.')
-            person = session.query(Person).filter(Person.pk = person_id).first()
+            person_id = unidecode(name.lower().replace(' ', '.'))
+            person = session.query(Person).filter(Person.facebook == user_id).first()
             if person == None:
-                session.add(Person(pk = person_id, facebook = user_id))
-                session.commit()
+                session.merge(Person(pk = person_id, facebook = user_id))
                 logger.info('Added %s' % person_id)
+            completed.add(name)
+    session.commit()
