1488cd4e87b087447ab6a0796028dfae953eb33b
Thomas Levine <_@thomaslevine.com>
2014-09-24 16:24:52 +0000

    fix foreign key

--- warehouse/notmuch/model.py
+++ warehouse/notmuch/model.py
@@ -17,14 +17,16 @@ class EmailCorrespondance(m.Fact):
     to_address includes CC, BCC
     '''
     pk = m.PkColumn()
-    message_id = m.FkColumn(EmailMessage.message_id)
+    message_id = m.Column(s.String, s.ForeignKey(EmailMessage.message_id))
     from_name = m.Column(s.String)
     from_address = m.Column(s.String)
     to_name = m.Column(s.String)
     to_address = m.Column(s.String)
 
 class EmailAttachment(m.Fact):
-    message_id = m.FkColumn(EmailMessage.message_id, primary_key = True)
+    message_id = m.Column(s.String,
+                          s.ForeignKey(EmailMessage.message_id),
+                          primary_key = True)
     message = relationship(EmailMessage)
     part_number = m.Column(s.Integer, primary_key = True)
     content_type = m.Column(s.String, nullable = True)
--- warehouse/twitter/model.py
+++ warehouse/twitter/model.py
@@ -10,5 +10,6 @@ class TwitterAction(m.Fact):
     user_handle = s.Column(s.String)
     user_name = s.Column(s.String)
     datetime = m.Column(s.DateTime)
-    action = s.Column(s.Enum('mention','favorite','retweet','follow','photo'))
+    action = s.Column(s.Enum('mention','favorite','retweet','follow','photo',
+                             name = 'twitter_action'))
     message_id = s.Column(s.String)
