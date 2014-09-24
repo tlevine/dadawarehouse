414438780c89ed006185628a27a626b46d7bb517
Thomas Levine <_@thomaslevine.com>
2014-09-24 18:34:23 +0000

    nfsn works

--- warehouse/piwik/nfsn.py
+++ warehouse/piwik/nfsn.py
@@ -5,8 +5,8 @@ def mysqldump(local_filename):
     if key not in os.environ:
         raise EnvironmentError('You must set the %s variable' % key)
 
-    remote_command = 'mysqldump --host=thomaslevine.db' \
-                     '--user=tlevine --password=\'%s\' piwik' % \
+    remote_command = 'mysqldump --host=thomaslevine.db ' \
+                     '--user=export --password=\'%s\' piwik' % \
                      os.environ[key]
 
     with open(local_filename, 'w') as fp:
