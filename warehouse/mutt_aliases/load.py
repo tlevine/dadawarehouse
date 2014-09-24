da030697c039a96c70f106f3f08573a9a20723b4
Thomas Levine <_@thomaslevine.com>
2014-09-24 19:14:12 +0000

    load mutt aliases

--- warehouse/mutt_aliases/load.py
+++ warehouse/mutt_aliases/load.py
@@ -12,14 +12,25 @@ def update(session):
 
 def mutt_aliases():
     fn = os.path.expanduser('~/git/secrets-home/.mutt/aliases/people')
+    identifiers = set()
     with open(fn, 'r') as fp:
         for line in fp:
             _, _, alias = line.partition(' ')
             identifier, _, name_address = alias.partition(' ')
+
+            if identifier in identifiers:
+                logger.info('Skipping duplicate entry for %s' % identifier)
+                continue
+
             m = re.match(r'([^<]+)<([^>]+)>', name_address)
-            if m == None:
+            if m != None:
+                name, address = m.group(1), m.group(2)
+            elif name_address.count(' ') == 0:
+                name = ''
+                address = name_address
+            else:
                 raise ValueError('This address is unusual; fix it:\n%s' % name_address)
-            name, address = m.group(1), m.group(2)
             yield MuttAlias(pk = identifier,
                             name = name,
                             email_address = address)
+            identifiers.add(identifier)
---
+++ warehouse/mutt_aliases/model.py
@@ -0,0 +1,8 @@
+import sqlalchemy as s
+
+from ..model import Fact, Column
+
+class MuttAlias(Fact):
+    pk = Column(s.String, primary_key = True)
+    name = Column(s.String)
+    email_address = Column(s.String)
