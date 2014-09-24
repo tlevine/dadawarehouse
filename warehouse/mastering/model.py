b513bf0f86296d2c872d0eb510d9d129b91cbcd6
Thomas Levine <_@thomaslevine.com>
2014-09-24 15:13:58 +0000

    master model

--- warehouse/mastering/model.py
+++ warehouse/mastering/model.py
@@ -1,4 +1,13 @@
+import sqlalchemy as s
+from sqlalchemy.orm import relationship, backref
 import ..model as m
 
-class Master(m.Dimension):
+class EmailAddress(m.Fact):
+    person_id = m.Column(s.String, s.ForeignKey(Person.pk))
+    value = m.Column(s.String)
 
+class Person(m.Dimension):
+    pk = m.Column(s.String, primary_key = True) # from the mutt alias file
+    facebook = m.Column(s.BigInteger, nullable = True)
+    twitter = m.Column(s.String, nullable = True)
+    email_addresses = relationship(EmailAddress)
