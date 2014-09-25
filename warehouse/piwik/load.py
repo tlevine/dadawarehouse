37aa94d5df4d0b50b8668e50b66592ee2851d5a9
Thomas Levine <_@thomaslevine.com>
2014-09-25 00:09:02 +0000

    piwik visits...

--- warehouse/piwik/load.py
+++ warehouse/piwik/load.py
@@ -1,10 +1,49 @@
 import os
+import datetime
 
-from ..util import i_should_copy
-from .nfsn import mysqldump, restore
+from lxml.etree import fromstring
+import requests
 
-LOCAL_DUMP = os.path.expanduser('~/.dadawarehouse/piwik.sql')
 def update(session):
-   #if i_should_copy(LOCAL_DUMP):
-   #    mysqldump(LOCAL_DUMP)
-    restore(LOCAL_DUMP)
+    key = 'PIWIK_API_TOKEN'
+    if key not in os.environ:
+        raise ValueError('You need to set the %s environment variable.' % key)
+
+    # The most recent date
+    date = session.query(PiwikVisit.date).order_by(desc(PiwikVisit.date)).scalar()
+
+    # Delete things from the most recent date in case of partial dates.
+    session.query(PiwikVisit).filter(PiwikVisit.date == date).delete()
+    session.commit()
+
+    while date <= datetime.date.today():
+        response = get_visits(os.environ[key], date)
+        session.add_all(parse_visits(response))
+        session.commit()
+        date += date + datetime.timedelta(days = 1)
+
+def visits(token, date):
+    for offset in itertools.count(0, 100):
+        response = get_visits(token, date, offset)
+        visits = map(reify_visit, json.loads(response.text))
+        yield from visits
+        if len(visits) == 0:
+            break
+
+def get_visits(token, date, page): 
+    url = 'http://piwik.thomaslevine.com'
+    params = {
+        'module': 'API',
+        'method': 'Live.getLastVisitsDetails',
+        'idSite': '2',
+        'period': 'day',
+        'date': date.isoformat(),
+        'format': 'xml',
+        'token_auth': token,
+        'filter_limit': 100,
+        'filter_offset': offset,
+    }
+    return requests.get(url, params = params)
+
+def reify_visit(blob):
+    pass
