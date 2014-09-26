fb6638bdaebe90447513a99dab213dd8346bb152
Thomas Levine <_@thomaslevine.com>
2014-09-26 16:18:04 +0000

    -

--- datawarehouse/__init__.py
+++ datawarehouse/__init__.py
@@ -1,1 +1,16 @@
+import csv
+
 from .person import Person, Facebook, Twitter, Name, EmailAddress, IPAddress
+
+file_mapping = [
+    ('person.csv', Person),
+    ('facebook.csv', Facebook),
+    ('name.csv', Name),
+    ('emailaddress.csv', EmailAddress),
+    ('ipaddress.csv', IPAddress),
+]
+def load(session):
+    for filename, Class in file_mapping:
+        with open(filename) as fp:
+            reader = csv.DictReader(fp)
+            session.add_all(Class(**row) for row in reader)
