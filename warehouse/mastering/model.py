d8d8fbafac133e9e898415b98f3156daf18f19bc
Thomas Levine <_@thomaslevine.com>
2014-09-26 02:34:47 +0000

    mastering model

--- warehouse/mastering/model.py
+++ warehouse/mastering/model.py
@@ -4,20 +4,57 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from ..model import Base, Column
 
-class Person(Base):
-    __tablename__ = 'master_person'
-    pk = Column(s.String, primary_key = True) # from the mutt alias file
-    facebook = Column(s.BigInteger, nullable = True)
-    twitter = Column(s.String, nullable = True)
-    names = relationship('Names')
-    email_addresses = relationship('EmailAddress')
+def GidColumn():
+    return Column(s.String)
 
-class Names(Base):
-    __tablename__ = 'master_names'
-    name = Column(s.String, primary_key = True)
-    person_id = Column(s.String, s.ForeignKey(Person.pk), primary_key = True)
+class PersonIdentifier(Base):
+    '''
+    Map between service-specific identifiers ("local_id")
+    and global identifiers ("global_id"), for different
+    services ("service").
 
-class EmailAddress(Base):
-    __tablename__ = 'master_emailaddress'
-    email_address = Column(s.String, primary_key = True)
-    person_id = Column(s.String, s.ForeignKey(Person.pk))
+    The global identifier is in the format that I use in
+    my mutt alias file, which is approximately one of these.
+
+    * ``[first name].[last name]``
+    * ``[first name].[middle name].[last name]``
+    * ``[first name].[last name].[context, like a city]``
+
+    I use the last of these if the others would be ambiguous
+    because different people have the same name.
+    '''
+    __abstract__ = True
+
+    @declared_attr
+    def __tablename__(Class):
+        return 'master_' + Class.__name__.lower()
+
+class ProtoMaster(Base):
+    '''
+    A place to store the guesses of what the master data might be
+    '''
+    __tablename__ = 'proto_master'
+    pk = PkColumn()
+    service = Column(s.String)
+    global_id = GidColumn()
+    local_id = Column(s.BigInteger)
+
+class Facebook(PersonIdentifier):
+    pk = PkColumn()
+    global_id = GidColumn()
+    local_id = Column(s.BigInteger, unique = True)
+
+class Twitter(PersonIdentifier):
+    pk = PkColumn()
+    global_id = GidColumn()
+    local_id = Column(s.String, unique = True)
+
+class Name(PersonIdentifier):
+    pk = PkColumn()
+    global_id = GidColumn()
+    name = Column(s.String)
+
+class EmailAddress(PersonIdentifier):
+    pk = PkColumn()
+    global_id = GidColumn()
+    email_address = Column(s.String, unique = True)
