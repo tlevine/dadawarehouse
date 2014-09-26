42c4169455e7088296f0ac28d2e7cf7939961a69
Thomas Levine <_@thomaslevine.com>
2014-09-26 03:41:11 +0000

    use dimensions

--- warehouse/mastering/model.py
+++ warehouse/mastering/model.py
@@ -1,33 +1,26 @@
+'''
+Mappings between service-specific identifiers ("local_id")
+and global identifiers ("global_id") for different services
+
+The global identifier is in the format that I use in
+my mutt alias file, which is approximately one of these.
+
+* ``[first name].[last name]``
+* ``[first name].[middle name].[last name]``
+* ``[first name].[last name].[context, like a city]``
+
+I use the last of these if the others would be ambiguous
+because different people have the same name.
+'''
+
 import sqlalchemy as s
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
-from ..model import Base, Column
+from ..model import Base, Dimension, Column
 
 def GidColumn():
     return Column(s.String)
-
-class PersonIdentifier(Base):
-    '''
-    Map between service-specific identifiers ("local_id")
-    and global identifiers ("global_id"), for different
-    services ("service").
-
-    The global identifier is in the format that I use in
-    my mutt alias file, which is approximately one of these.
-
-    * ``[first name].[last name]``
-    * ``[first name].[middle name].[last name]``
-    * ``[first name].[last name].[context, like a city]``
-
-    I use the last of these if the others would be ambiguous
-    because different people have the same name.
-    '''
-    __abstract__ = True
-
-    @declared_attr
-    def __tablename__(Class):
-        return 'master_' + Class.__name__.lower()
 
 class ProtoMaster(Base):
     '''
@@ -39,22 +32,22 @@ class ProtoMaster(Base):
     global_id = GidColumn()
     local_id = Column(s.BigInteger)
 
-class Facebook(PersonIdentifier):
+class Facebook(Dimension):
     pk = PkColumn()
     global_id = GidColumn()
     local_id = Column(s.BigInteger, unique = True)
 
-class Twitter(PersonIdentifier):
+class Twitter(Dimension):
     pk = PkColumn()
     global_id = GidColumn()
     local_id = Column(s.String, unique = True)
 
-class Name(PersonIdentifier):
+class Name(Dimension):
     pk = PkColumn()
     global_id = GidColumn()
     name = Column(s.String)
 
-class EmailAddress(PersonIdentifier):
+class EmailAddress(Dimension):
     pk = PkColumn()
     global_id = GidColumn()
     email_address = Column(s.String, unique = True)
