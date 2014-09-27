026d93db1b1f904b7139f6711695ca262f8ae9d1
Thomas Levine <_@thomaslevine.com>
2014-09-27 11:02:46 +0000

    adding joins

--- datawarehouse/person.py
+++ datawarehouse/person.py
@@ -24,6 +24,7 @@ from datamarts import (
     MuttAlias,
     NotmuchMessage, NotmuchRecipient, NotmuchAttachment,
     PiwikVisit,
+    TwitterAction
 )
 
 def Array(columntype):
@@ -31,22 +32,18 @@ def Array(columntype):
 
 class Person(Fact):
     id = Column(s.String, primary_key = True)
-    facebooks = Array(s.BigInteger)
-    twitters = Array(s.String)
     email_addresses = Array(s.String)
     names = Array(s.String)
     ip_addresses = Array(CIDR)
     piwiks = Array(s.String)
-
-FacebookMessage.add_join([(FacebookMessage.user_id, Person.facebooks)])
-FacebookChatStatusChange.add_join([(FacebookChatStatusChange.user_id,
-                                    Person.facebooks)])
-FacebookDuration.add_join([(FacebookDuration.user_id, Person.facebooks)])
-FacebookNameChange.add_join([(FacebookNameChange.user_id, Person.facebooks)])
+    facebooks = Array(s.BigInteger)
+    twitters = Array(s.String)
+    mutt = Column(s.String)
 
 NotmuchMessage.add_join([(NotmuchMessage.from_address, Person.email_addresses)])
 NotmuchMessage.add_join([(NotmuchMessage.recipient_address, Person.email_addresses)])
 
+# Match on names for email addresses in case we don't have an entity yet
 NotmuchMessage.add_join([(NotmuchMessage.from_name, Person.names)])
 NotmuchMessage.add_join([(NotmuchMessage.recipient_name, Person.names)])
 
@@ -54,3 +51,13 @@ BranchableLog.add_join([(BranchableLog.ip_address, Person.ip_addresses)])
 
 PiwikVisit.add_join([(PiwikVisit.visitorId, Person.piwiks)])
 PiwikVisit.add_join([(PiwikVisit.visitIp, Person.ip_addresses)])
+
+TwitterAction.add_join([(TwitterAction.user_handle, Person.twitters)])
+
+FacebookMessage.add_join([(FacebookMessage.user_id, Person.facebooks)])
+FacebookChatStatusChange.add_join([(FacebookChatStatusChange.user_id,
+                                    Person.facebooks)])
+FacebookDuration.add_join([(FacebookDuration.user_id, Person.facebooks)])
+FacebookNameChange.add_join([(FacebookNameChange.user_id, Person.facebooks)])
+
+Person.add_join([(
