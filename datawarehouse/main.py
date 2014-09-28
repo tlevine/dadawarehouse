5dfc3293ddb2082f8b492410832ae943d71cc677
Thomas Levine <_@thomaslevine.com>
2014-09-28 15:17:15 +0000

    notmuch names and todo list

--- datawarehouse/main.py
+++ datawarehouse/main.py
@@ -1,13 +1,15 @@
 import os
 
+from sqlalchemy.orm import sessionmaker
+
 from doeund import make_cubes
 
 from .person.load import load_person
 
 def load(engine):
     directory = os.path.expanduser('~/git/dadawarehouse-manual')
-    load_person(directory, engine)
     session = sessionmaker(engine)()
+    load_person(session, directory)
 
     for statement in make_cubes():
         session.execute(statement)
--- datawarehouse/person/load.py
+++ datawarehouse/person/load.py
@@ -2,7 +2,6 @@ import os
 import csv
 from collections import defaultdict
 
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 from datamarts import (
@@ -11,29 +10,48 @@ from datamarts import (
     MuttAlias,
     NotmuchMessage,
     PiwikVisit,
-    TwitterAction
+    TwitterAction,
 )
 
-from .model import Person
+from .model import Person, EmailAddress, Facebook, Twitter
 
 file_mapping = [
-    ('emailaddress.csv', 'email_addresses', EmailAddress),
-    ('facebook.csv', 'facebooks', Facebook),
-    ('twitter.csv', 'twitters', Twitter),
-    ('name.csv', 'names', None),
-    ('ipaddress.csv', 'ip_addresses', None),
-    ('piwik.csv', 'piwiks', None),
+    ('emailaddress.csv', Person.email_addresses, EmailAddress),
+    ('facebook.csv', Person.facebooks, Facebook),
+    ('twitter.csv', Person.twitters, Twitter),
+    ('name.csv', Person.names, None),
+    ('ipaddress.csv', Person.ip_addresses, None),
+    ('piwik.csv', Person.piwiks, None),
 ]
 
 def _strip(dictionary):
     return {k.strip():v.strip() for k,v in dictionary.items()}
 
 def load_notmuch(session):
-    session.query(Notmuch
+    name_addresses = session.query(NotmuchMessage.from_name,
+                                   NotmuchMessage.from_address).distinct()
+    for name, address in name_addresses:
+        person = session.query(Person)\
+                        .join(EmailAddress)\
+                       #.join(EmailAddress,
+                       #      Person.id == EmailAddress.person_id)\
+                        .filter(EmailAddress.emailaddress == address)\
+                        .first()
+        person.email_addresses = set(person.email_addresses).union({address})
+        person.names = set(person.names).union({name})
+        session.flush()
 
-def load_csv(directory, engine):
-    session = sessionmaker(bind=engine)()
-    for filename, column_name, Class in file_mapping:
+def load_branchable():
+def load_facebook():
+def load_muttalias():
+def load_piwik():
+def load_twitter():
+
+def load_sql(session):
+    load_notmuch(session)
+
+def load_csv(session, directory):
+    for filename, column, Class in file_mapping:
         path = os.path.join(directory, filename)
         if os.path.exists(path):
             with open(path) as fp:
@@ -45,17 +63,15 @@ def load_csv(directory, engine):
 
                 person_sets = defaultdict(lambda: set())
                 for row in rows:
-                    person_sets[row['person_id']].add(row['value'])
+                    person_sets[row['person_id']].add(row[column.name])
                 for person_id, local_ids in person_sets.items():
                     session.query(Person)\
-                           .filter(id = row['person_id'])\
-                           .update({column_name: local_ids})
+                           .filter(id == row['person_id'])\
+                           .update({column: local_ids})
 
                 if Class != None:
                     session.query(Class).delete()
-                    records = (Class(person_id = row['person_id'],
-                                     id = row['value'])\
-                               for row in rows)
+                    records = (Class(**row) for row in rows)
                     session.add_all(records)
 
         else:
