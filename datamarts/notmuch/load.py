83d38c92af3fbf7b57276337e6eb75cf567f6e30
Thomas Levine <_@thomaslevine.com>
2014-09-29 04:32:30 +0000

    unnecessary multithreading

--- datamarts/notmuch/load.py
+++ datamarts/notmuch/load.py
@@ -4,6 +4,7 @@ import re
 import datetime
 from itertools import chain
 import subprocess
+from functools import partial
 from concurrent.futures import ThreadPoolExecutor
 
 from notmuch import Database, Query
@@ -17,29 +18,39 @@ from .model import NotmuchMessage, NotmuchAttachment
 def update(sessionmaker):
     if offlineimap_is_running():
         raise EnvironmentError('In case offlineimap runs "notmuch new", you should stop offlineimap while importing data from notmuch.')
-    db = Database()
     q = sessionmaker().query(NotmuchMessage.message_id)
     past_messages = set(row[0] for row in q.distinct())
-    with ThreadPoolExecutor(10) as e:
-        for m in Query(db,'').search_messages():
-            message_id = m.get_message_id()
-            if message_id in past_messages:
-                logger.debug('Already imported %s' % message_id)
-            else:
-                past_messages.add(message_id)
-                future = e.submit(add_message, sessionmaker, past_messages, m)
-                future.add_done_callback(_raise_in_future)
 
+    # This would make things faster if my emails were on different SSDs.
+    _update = partial(update_from_query, past_messages, sessionmaker)
+    with ThreadPoolExecutor(25) as e:
+        future = e.submit(_update, 'date:..2005')
+        future.add_done_callback(_raise)
+        for year in range(2006, datetime.date.today().year):
+            future = e.submit(_update, 'date:%d..%d' % (year, year))
+            future.add_done_callback(_raise)
 
+def _raise(future):
+    exception = future.exception()
+    if exception != None:
+        raise exception
+
+def update_from_query(past_messages, sessionmaker, querystr):
+    db = Database()
     session = sessionmaker()
-    session.add(message(m))
-    session.flush() # for foreign key constraints
-    session.add_all(attachments(m))
+    for m in Query(db,querystr).search_messages():
+        message_id = m.get_message_id()
+        if message_id in past_messages:
+            logger.debug('Already imported %s' % message_id)
+            continue
+        past_messages.add(message_id)
 
-    past_messages.add(m.get_message_id())
-    session.commit()
+        session.add(message(m))
+        session.flush() # for foreign key constraints
+        session.add_all(attachments(m))
 
-    logger.info('Added message "id:%s"' % m.get_message_id())
+        session.commit()
+        logger.info('Added message "id:%s"' % m.get_message_id())
 
 def offlineimap_is_running():
     pgrep = subprocess.Popen(['pgrep', 'offlineimap'], stdout = subprocess.PIPE)
