173b46b6363f129a8f8aa5dc23d8d9a55cd73af1
Thomas Levine <_@thomaslevine.com>
2014-09-30 19:28:34 +0000

    yay dates

--- datamarts/wtmp/parse.py
+++ datamarts/wtmp/parse.py
@@ -20,14 +20,14 @@ datetime = p.Group(p.Word(p.alphas, exact=3).setResultsName('day_of_week') +
 
 parser = p.Word(p.alphanums).setResultsName('user') + \
          p.Word(p.alphanums + '/').setResultsName('tty') + \
+         p.restOfLine
+         '''
          datetime.setResultsName('login_datetime') + \
          p.Suppress('-') + \
          datetime.setResultsName('logout_datetime') + \
          duration + \
          ip_address + p.restOfLine
-
-line = 'Fri Aug  1 10:05:24 2014'
-print(datetime.parseString(line))
+         '''
 
 line = 'tlevine  pts/7        Fri Aug  1 10:05:24 2014 - Fri Aug  1 10:15:07 2014  (00:09)     178.36.15.241 via mosh [30685]\n'
 print(parser.parseString(line))
