c15de1aa5ddad5c9e9e66fb47f3109afe2fc6f46
Thomas Levine <_@thomaslevine.com>
2014-09-30 16:53:59 +0000

    order things

--- datamarts/ssh/load.py
+++ datamarts/ssh/load.py
@@ -1,19 +1,25 @@
 from subprocess import Popen, PIPE
 
-def shell_command(f):
+def update(session):
+    for host in ['nsa', 'home']:
+        for filename in ls(host):
+            print(host, filename)
+            print(len(list(last(host, filename))))
+
+
+def shell(f):
     def wrapper(*args, **kwargs):
         command = f(*args, **kwargs)
         sp = Popen(command, stdout = PIPE, stderr = PIPE)
-        sp.wait()
         stdout, stderr = sp.communicate()
-        return stdout.split('\n')
+        sp.wait()
+        return filter(None, stdout.decode('utf-8').split('\n'))
     return wrapper
 
+@shell
 def ls(host):
     return ['ssh', host, 'ls /var/log/wtmp*']
 
+@shell
 def last(host, filename):
     return ['ssh', host, "last -F -a -f '%s'" % filename]
-
-for filename in ls('nsa'):
-    print(last('nsa', filename))
