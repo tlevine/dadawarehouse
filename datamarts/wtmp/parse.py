d644d336d1e0c3043a931540ff94da6b67fab35c
Thomas Levine <_@thomaslevine.com>
2014-09-30 19:09:02 +0000

    write a parser

--- datamarts/wtmp/parse.py
+++ datamarts/wtmp/parse.py
@@ -1,3 +1,27 @@
-line = 'tlevine  pts/7        Fri Aug  1 10:05:24 2014 - Fri Aug  1 10:15:07 2014  (00:09)     178.36.15.241 via mosh [30685]\n'
+import string
 
-def parse(line):
+import pyparsing as p
+
+day_of_week = p.Word(string.ascii_uppercase, string.ascii_lowercase, exact=3)
+month = p.Word(string.ascii_uppercase, string.ascii_lowercase, exact=3)
+duration = p.Combine('(' + integer + ':' + integer + ')')
+ip_address = p.Combine(integer + '.' + integer + '.' +
+                       integer + '.' + integer + '.')
+
+datetime = p.Group(day_of_week + 
+                   month +
+                   integer + 
+                   p.Combine(integer + ':' + integer + ':' integer) +
+                   integer)
+
+parser = p.Word() + p.Word() +
+         datetime +
+         p.Suppress('-') +
+         datetime +
+         duration +
+         ip_address + 
+         p.restOfLine
+
+
+line = 'tlevine  pts/7        Fri Aug  1 10:05:24 2014 - Fri Aug  1 10:15:07 2014  (00:09)     178.36.15.241 via mosh [30685]\n'
+print(parser.parseString(line))
