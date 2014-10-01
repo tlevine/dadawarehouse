22884839f8cda56352d2b17421d04404562ce9a2
Thomas Levine <_@thomaslevine.com>
2014-10-01 03:38:09 +0000

    stuff

--- mastering/piwik_email/load.py
+++ mastering/piwik_email/load.py
@@ -10,6 +10,12 @@ def piwik_email(sessionmaker):
                                            PiwikVisitorDays.date,
                                            PiwikVisitorDays.visitor_id)
 
+    for email_address, email_dates in email_address_days:
+        for piwik_visitorid, piwik_dates in piwik_visitorid_days:
+            intersection = len(email_dates.intersection(piwik_dates))
+            union = len(email_dates.union(piwik_dates))
+            intersection / union
+
 def identifier_sets(session, date_column, identifier_column)
     identifier_days = defaultdict(set)
     query = session.query(date_column, identifier_column)
