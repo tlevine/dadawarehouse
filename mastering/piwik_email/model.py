99273c88f14a0a41c4d9adca93f16944f542909a
Thomas Levine <_@thomaslevine.com>
2014-10-01 08:50:31 +0000

    remove views stuff

--- mastering/piwik_email/model.py
+++ mastering/piwik_email/model.py
@@ -2,16 +2,6 @@ import sqlalchemy as s
 
 from doeund import Fact, Column
 
-class EmailAddressDays(Fact):
-    'This should just be a view.'
-    date = Column(s.Date, primary_key = True)
-    email_address = Column(s.String, primary_key = True)
-
-class PiwikVisitorDays(Fact):
-    'This should just be a view.'
-    date = Column(s.Date, primary_key = True)
-    visitor_id = Column(s.String, primary_key = True)
-
 class PiwikEmailOverlap(Fact):
     visitor_id = Column(s.String, primary_key = True)
     email_address = Column(s.String, primary_key = True)
