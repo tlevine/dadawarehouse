215b3c8e4a77d75e58ae9019b7bc3f983f7f5d64
Thomas Levine <_@thomaslevine.com>
2014-10-01 04:25:13 +0000

    thinking

--- mastering/piwik_email/match-piwik-email.sql
+++ mastering/piwik_email/match-piwik-email.sql
@@ -5,8 +5,32 @@ SELECT DISTINCT
   "visitorId" AS "piwik_id"
 FROM ft_piwikvisit;
 
-CREATE TABLE "ft_emailaddress_days" AS
+-- Dates when emailers emailed
+CREATE VIEW "ft_emailaddress_days" AS
 SELECT DISTINCT
   date("datetime") AS "date",
   "from_address" AS "email_address"
 FROM ft_notmuchmessage;
+
+-- Returning visitors
+CREATE VIEW "helper_returning_visitors" AS
+SELECT "piwik_visitorid"
+FROM "ft_piwikvisitor_days"
+GROUP BY "piwik_visitorid"
+WHERE count(*) >= 2;
+
+-- Returning emailers
+CREATE VIEW "helper_returning_emailers" AS
+SELECT "email_address"
+FROM "ft_emailaddress_days"
+GROUP BY "email_address"
+WHERE count(*) >= 2;
+
+-- Maybe I need a windowed query for this...
+-- I want to convert the Python in load.py to SQL.
+SELECT *
+FROM "helper_returning_visitors"
+JOIN "ft_piwikvisitor_days"
+ON "helper_returning_visitors"."piwik_visitorid" 
+
+CREATE VIEW "ft_piwik_email_overlap" AS
